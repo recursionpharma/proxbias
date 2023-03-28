df773ad87a415859b1b6aef7f27651761d9eea94
John Urbanik <john.urbanik@recursionpharma.com>
2023-03-28 00:28:18 -0400

    Add extra test with warning

--- proxbias/utils.py
+++ proxbias/utils.py
@@ -50,20 +50,22 @@ def get_chromosome_info_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame, pd.Data
         centromere_start=("chromStart", "min"),
         centromere_end=("chromEnd", "max"),
     )
+
+    chroms = chroms.loc[chroms.chrom.isin(VALID_CHROMS)].rename(columns={"chromStart": "start", "chromEnd": "end"})
+    chroms = chroms.merge(centros, on="chrom", how="left")
+    chroms["chrom_int"] = _chrom_int(chroms.chrom)
+    chroms = chroms.set_index("chrom").sort_values("chrom_int", ascending=True)
+
     bands = (
         bands.rename(columns={"#chrom": "chrom"})
         .groupby(["chrom", "name"], as_index=False)
         .agg(
             band_start=("chromStart", "min"),
             band_end=("chromEnd", "max"),
+            band_chrom_arm=("name", lambda x: x.str[:1].min()),
         )
     )
-    bands.chrom_int = _chrom_int(bands.chrom)
-
-    chroms = chroms.loc[chroms.chrom.isin(VALID_CHROMS)].rename(columns={"chromStart": "start", "chromEnd": "end"})
-    chroms = chroms.merge(centros, on="chrom", how="left")
-    chroms["chrom_int"] = _chrom_int(chroms.chrom)
-    chroms = chroms.set_index("chrom").sort_values("chrom_int", ascending=True)
+    bands["chrom_int"] = _chrom_int(bands.chrom)
 
     genes = pd.read_csv(
         DATA_DIR + "/ncbirefseq_hg38.tsv.gz", sep="\t", usecols=["name2", "chrom", "txStart", "txEnd"]
@@ -77,11 +79,11 @@ def get_chromosome_info_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame, pd.Data
         chrom_int=("chrom_int", "min"),
         chrom_count=("chrom", "nunique"),
     )
-    genes = genes.loc[genes.chrom_count == 1].set_index("gene")
+    genes = genes.loc[genes.chrom_count == 1].drop(columns="chrom_count").set_index("gene")
     genes = genes.sort_values(["chrom_int", "start", "end"], ascending=True)
 
     chrom_centromere = chroms.set_index("chrom_int").centromere_start.to_dict()
-    genes = genes.assign(chrom_arm=genes.apply(lambda x: x.start > chrom_centromere[x.chrom_int], axis=1).astype(int))
+    genes["chrom_arm_int"] = genes.apply(lambda x: x.end > chrom_centromere[x.chrom_int], axis=1).astype(int)
     return genes, chroms, bands
 
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,62 @@
+import warnings
+
+import numpy as np
+import pandas as pd
+
 from proxbias.utils import get_chromosome_info_dataframe, get_chromosome_info_dict
 
 
 def test_keys_equal():
     gene_df, chrom_df, band_df = get_chromosome_info_dataframe()
     gene_dict, chrom_dict, _, band_dict = get_chromosome_info_dict()
-    print(chrom_df)
     assert set(gene_df.index) == set(gene_dict.keys())
     assert set(chrom_df.index) == set(chrom_dict.keys())
     assert len(band_df.index) == len(band_dict.keys())
+
+
+def test_coordinates_equal():
+    gene_df, chrom_df, band_df = get_chromosome_info_dataframe()
+    gene_dict, chrom_dict, _, band_dict = get_chromosome_info_dict()
+
+    gene_df2 = pd.DataFrame.from_dict(gene_dict).T.sort_index()
+    gene_df2[["start", "end"]] = gene_df2[["start", "end"]].astype(np.int64)
+    gene_df["chrom"] = gene_df["chrom_int"].apply(lambda x: f"chr{x}" if x < 23 else ("chrX" if x == 24 else "chrY"))
+    gene_df["arm"] = gene_df["chrom_arm_int"].apply(lambda x: "p" if x == 0 else "q")
+    gene_df["arm"] = gene_df.apply(lambda x: f"{x.chrom}{x.arm}", axis=1)
+    gene_df = gene_df[["chrom", "start", "end", "arm"]].sort_index()
+
+    try:
+        pd.testing.assert_frame_equal(
+            gene_df,
+            gene_df2,
+            check_names=False,
+            check_like=True,
+        )
+    except AssertionError:
+        differences1 = gene_df.loc[gene_df["arm"] != gene_df2["arm"]]
+        differences2 = gene_df.loc[gene_df["arm"] != gene_df2["arm"]]
+        if len(differences1) < 10:
+            warnings.warn(
+                f"Gene data differed by {len(differences1)} rows (less than the maximum 10):"
+                f"\n\n{differences1} \n\nvs\n\n{differences2}"
+            )
+
+    chrom_df2 = pd.DataFrame.from_dict(chrom_dict).T
+    pd.testing.assert_frame_equal(
+        chrom_df.drop(columns="chrom_int"),
+        chrom_df2,
+        check_names=False,
+        check_like=True,
+    )
+
+    band_df2 = pd.DataFrame.from_dict(band_dict).T
+    band_df2.columns = ["chrom", "band_start", "band_end"]
+    band_df2[["band_start", "band_end"]] = band_df2[["band_start", "band_end"]].astype(np.int64)
+    band_df["combined"] = band_df.chrom + band_df.name
+    band_df = band_df.set_index("combined").drop(columns=["band_chrom_arm", "chrom_int", "name"])
+    pd.testing.assert_frame_equal(
+        band_df,
+        band_df2,
+        check_names=False,
+        check_like=True,
+    )
