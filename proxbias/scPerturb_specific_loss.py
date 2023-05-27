a3958e1726458dea42e011789030b8c7ddd5892d
Safiye Celik <safiye.celik@scelik-mbp.rxrx.io>
2023-05-26 23:27:59 -0400

    add plotting helper functions

--- proxbias/scPerturb_specific_loss.py
+++ proxbias/scPerturb_specific_loss.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import wget
+import re
 import scanpy
 import numpy as np
 import pandas as pd
@@ -33,10 +34,10 @@ def get_telo_centro(arm: str, direction: str) -> str:
         >>> get_telo_centro('Xq', '3prime')
         'telomeric'
     """
-    if arm[-1] == 'p':
-        return 'centromeric' if '3' in direction else 'telomeric'
-    if arm[-1] == 'q':
-        return 'telomeric' if '3' in direction else 'centromeric'
+    if arm[-1] == "p":
+        return "centromeric" if "3" in direction else "telomeric"
+    if arm[-1] == "q":
+        return "telomeric" if "3" in direction else "centromeric"
 
 
 def compute_loss_w_specificity(
@@ -216,3 +217,174 @@ def load_and_process_data(filename: str, chromosome_info: pd.DataFrame) -> AnnDa
         ad.obs.loc[ad.obs.perturbation == "control", "perturbation_label"] = "control"
 
     return ad[ad.obs.perturbation_label != ""]
+
+
+def generate_save_summary_results(
+    filenames, blocksize: int = 5, window: int = 100, neigh: int = 150, zscore_cutoff: float = 3.0
+):
+    spec_genes_dict = {}
+    for filename in filenames:
+        res = pd.read_csv(f"f{filename}_b{blocksize}_w{window}_n{neigh}.csv", index_col=0)
+        for c in ["3p", "5p"]:
+            col = f"loss{c}_cellfrac"
+            col2 = f"loss{c}_cellcount"
+            res_c = res[["aff_gene", "ko_gene", col, col2]]
+            res_trans = res_c.copy()
+            res_trans[col] = res_trans.groupby("aff_gene")[col].transform(lambda x: zscore(x))
+            res_trans = (
+                res_trans[(res_trans.aff_gene == res_trans.ko_gene) & (res_trans[col2] >= 1)]
+                .sort_values(by=col)
+                .reset_index(drop=True)
+            )
+            res_trans = res_trans[res_trans[col] >= zscore_cutoff]
+            spec_genes = list(res_trans.ko_gene)
+            spec_genes_dict[(filename, c)] = spec_genes
+
+    tested_gene_count_dict = {}
+    allres = []
+    for filename in filenames:
+        res = pd.read_csv(f"f{filename}_b{blocksize}_w{window}_n{neigh}.csv", index_col=0)
+        filename_short = re.findall("[A-Z][^A-Z]*", filename)[0]
+        tested_gene_count_dict[filename_short] = len(res.aff_gene.unique())
+        for c in ["3p", "5p"]:
+            col = f"loss{c}_cellfrac"
+            col2 = f"loss{c}_cellcount"
+            res_c = res[["aff_gene", "aff_arm", "ko_gene", "ko_arm", col, col2]]
+            specific_loss = res_c[
+                (res_c.aff_gene == res_c.ko_gene) & res_c.aff_gene.isin(spec_genes_dict[(filename, c)])
+            ]
+            tmp = specific_loss[["ko_gene", "ko_arm", col, col2]].rename(
+                columns={
+                    col: "% affected cells",
+                    col2: "# affected cells",
+                    "ko_gene": "Perturbed gene",
+                    "ko_arm": "Chr arm",
+                }
+            )
+            tmp["% affected cells"] = tmp["% affected cells"].apply(lambda x: round(x * 100, 2))
+            tmp["Dataset"] = filename_short
+            tmp["Perturbation type"] = (
+                "CRISPR-cas9" if filename_short in ["Frangieh", "Papalexi", "Dixit"] else "CRISPRi"
+            )
+            tmp["Tested loss direction"] = c.replace("p", "'")
+            allres.append(tmp.sort_values("% affected cells", ascending=False))
+
+    allres = pd.concat(allres)
+    allres["Total # cells"] = allres.apply(lambda x: int(x["# affected cells"] / x["% affected cells"] * 100), axis=1)
+    allres["Telomeric or centromeric"] = allres.apply(
+        lambda x: get_telo_centro(x["Chr arm"], x["Tested loss direction"]), axis=1
+    )
+    allres = allres[
+        [
+            "Perturbed gene",
+            "Perturbation type",
+            "Dataset",
+            "Chr arm",
+            "Tested loss direction",
+            "Total # cells",
+            "# affected cells",
+            "% affected cells",
+            "Telomeric or centromeric",
+        ]
+    ]
+    allres.to_csv("allres.csv", index=None)
+
+    gr_cols = ["Perturbation type", "Dataset", "Tested loss direction"]
+    summaryres = (
+        allres.groupby(gr_cols)
+        .agg({"Telomeric or centromeric": [len, lambda x: sum(x == "telomeric"), lambda x: sum(x == "centromeric")]})
+        .reset_index()
+    )
+    add_cols = [
+        "# targets w/ specific loss",
+        "# targets w/ loss towards telomere",
+        "# targets w/ loss towards centromere",
+    ]
+    summaryres.columns = gr_cols + add_cols
+    summaryres["Total # tested targets"] = summaryres["Dataset"].apply(lambda x: tested_gene_count_dict[x])
+    summaryres["% targets w/ specific loss"] = summaryres.apply(
+        lambda r: round((r["# targets w/ specific loss"] / r["Total # tested targets"]) * 100, 1), axis=1
+    )
+    summaryres = summaryres[gr_cols + ["% targets w/ specific loss", "Total # tested targets"] + add_cols]
+    summaryres.to_csv("summaryres.csv", index=None)
+
+
+def generate_plot_args():
+    perts2check_d = {}
+    plot_args = {}
+
+    plot_args[filename] = {}
+    ad = scanpy.read_h5ad(f"{filename}.h5ad")
+    filename_short = re.findall("[A-Z][^A-Z]*", filename)[0]
+    perts2check_df = allres[
+        (allres["Dataset"] == filename_short) & (allres["# affected cells"] >= cell_count_thrs[filename_short])
+    ]
+    perts2check = sorted(set(perts2check_df["Perturbed gene"]))
+    perts2check_d[filename] = perts2check
+
+    ad.var = ad.var.rename(columns={"start": "st", "end": "en"}).join(all_genes_bed_HGNC, how="left")
+    if filename.startswith("Papalexi"):
+        ad.obs["gene"] = (
+            ad.obs.perturbation.apply(lambda x: x.split("g")[0] if x != "control" else "").fillna("").astype(str)
+        )
+        print(ad.obs["gene"].unique())
+    elif filename.startswith("Dixit"):
+        ad = ad[
+            (ad.obs.perturbation == "control")
+            | ((ad.obs.perturbation.str.count("-") > 0) & ~ad.obs.perturbation.str.contains("INTERGENIC"))
+        ].copy()
+        ad.obs["gene"] = (
+            ad.obs.perturbation.apply(lambda x: x.split("-")[1].split("sg")[1] if x != "control" else "")
+            .fillna("")
+            .astype(str)
+        )
+    elif filename.startswith("Frangieh"):
+        ad.obs["gene"] = ad.obs.perturbation.apply(lambda x: x if x != "control" else "").fillna("").astype(str)
+    ad.obs.loc[ad.obs.perturbation == "control", "gene"] = "control"
+    ad = ad[ad.obs.gene.isin(perts2check + ["control"])]
+    infercnvpy.tl.infercnv(
+        ad, reference_key="gene", reference_cat="control", window_size=window, step=blocksize, exclude_chromosomes=None
+    )
+
+    res = pd.read_csv(f"f{filename}_b{blocksize}_w{window}_n{neigh}.csv", index_col=0)
+    loss_arrs = []
+    other_arrs = []
+    loss_seps = []
+    other_seps = []
+    blocknums = []
+    for p in perts2check:
+        res_p = res[(res.ko_gene == p) & (res.aff_gene == p)]
+        direcs = list(perts2check_df[perts2check_df["Perturbed gene"] == p]["Tested loss direction"])
+        loss_cell_inds = sum(
+            [[ad.obs.index.get_loc(x) for x in ast.literal_eval(res_p[f"loss{d[0]}p_cells"].iloc[0])] for d in direcs],
+            [],
+        )
+        other_cell_inds = list(
+            set(ad.obs.index.get_loc(x) for x in ad.obs.loc[ad.obs.gene == p].index).difference(loss_cell_inds)
+        )
+        arr1 = ad.obsm["X_cnv"].toarray()[loss_cell_inds]
+        arr2 = ad.obsm["X_cnv"].toarray()[other_cell_inds]
+        aff_chr = ad.var.loc[p].chromosome
+        aff_chr_startblocknum = ad.uns["cnv"]["chr_pos"][aff_chr]
+        blocknum_p = (
+            aff_chr_startblocknum
+            + ad.var[ad.var.chromosome == aff_chr].sort_values("start").index.get_loc(p) // blocksize
+        )
+        blocknums.append(blocknum_p)
+        loss_arrs = arr1 if len(loss_arrs) == 0 else np.concatenate((loss_arrs, arr1), axis=0)
+        other_arrs = arr2 if len(other_arrs) == 0 else np.concatenate((other_arrs, arr2), axis=0)
+        loss_seps.append(len(loss_cell_inds) if len(loss_seps) == 0 else len(loss_cell_inds) + loss_seps[-1])
+        other_seps.append(len(other_cell_inds) if len(other_seps) == 0 else len(other_cell_inds) + other_seps[-1])
+
+    plot_args[filename]["mid_tick"] = list(ad.uns["cnv"]["chr_pos"].values()) + [ad.obsm["X_cnv"].shape[1]]
+    plot_args[filename]["x_tick_lab"] = list(ad.uns["cnv"]["chr_pos"].keys())
+    plot_args[filename]["x_tick_loc"] = get_mid_ticks(
+        list(ad.uns["cnv"]["chr_pos"].values()) + [ad.obsm["X_cnv"].shape[1]]
+    )
+    plot_args[filename]["chr_pos"] = ad.uns["cnv"]["chr_pos"].values()
+    plot_args[filename]["loss_arrs"] = loss_arrs
+    plot_args[filename]["other_arrs"] = other_arrs
+    plot_args[filename]["loss_seps"] = loss_seps
+    plot_args[filename]["other_seps"] = other_seps
+    plot_args[filename]["blocknums"] = blocknums
+    return plot_args
