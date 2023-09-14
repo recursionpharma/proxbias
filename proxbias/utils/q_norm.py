e34c41faf4e3221d4aaa1cfb2d0db05b11b334f2
Nathan Lazar <nathan.lazar@gmail.com>
2023-09-14 12:16:52 -0600

    made q_norm a bit more efficient (#33)
    * made q_norm a bit more efficient
    
    * black likes double quotes better
    
    * bump version

--- proxbias/utils/q_norm.py
+++ proxbias/utils/q_norm.py
@@ -10,17 +10,7 @@ def _get_percentiles(
     """
     Convert values in `x` to percentiles relative to the `null`
     """
-    return np.searchsorted(null, x) / len(null)
-
-
-def _transform(
-    percentiles: np.ndarray,
-    norm: ss.norm,
-) -> np.ndarray:
-    """
-    transform `percentiles` into corresponding values in `norm`
-    """
-    return norm.ppf(percentiles)
+    return np.searchsorted(null, x, side="right") / len(null)
 
 
 def get_transforms(
@@ -35,22 +25,10 @@ def get_transforms(
     obtain each measurement's percentile ranking, and then rescale that
     percentile according to a normal function to mimic a normal distribution of
     transformed values.
-    Inputs:
-    -------
-    - x (np.ndarray): array of values
-    - null_distribution (np.ndarray): values that represents a null
-    - loc (float, optional): Used for the center of the normal distribution. Defaults to 0.0
-    - scale (int, optional): Used for the scale of the normal distribution. Defaults to 0.2
-    Returns:
-    --------
-    - np.ndarray
     """
-    norm = ss.norm(loc=loc, scale=scale)
-    null = null.copy()
     null.sort()
-    percs = _get_percentiles(null, x)
-    trans = _transform(percs, norm)
-    return trans
+    percs = _get_percentiles(x=x, null=null)
+    return ss.norm(loc=loc, scale=scale).ppf(percs)
 
 
 def q_norm(
@@ -60,10 +38,10 @@ def q_norm(
     """
     Quantile normalizes a square symmetrical dataframe to a normal distrbution
     """
-    X = np.zeros(df.shape)
     ind_u = np.triu_indices(df.shape[0], 1)
-    tmp_u = get_transforms(df.values[ind_u], df.values[ind_u], **trans_args)
-    X[ind_u] = tmp_u.clip(-1, 1)
-    df_norm = pd.DataFrame(X + X.T, index=df.index, columns=df.columns)
+    X = np.zeros(df.shape)
+    X[ind_u] = get_transforms(x=df.values[ind_u], null=df.values[ind_u], **trans_args)
+    X += X.T
+    df_norm = pd.DataFrame(X, index=df.index, columns=df.columns)
     np.fill_diagonal(df_norm.values, 1)
     return df_norm
