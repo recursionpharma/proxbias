d51c57d8c2f514f16b2455faa3d33d82431dda2d
Safiye Celik <safiye.celik@recursionpharma.com>
2024-05-13 08:52:34 -0400

    fix index

--- proxbias/scPerturb_processing_plotting.py
+++ proxbias/scPerturb_processing_plotting.py
@@ -70,7 +70,7 @@ def _compute_chromosomal_loss(
     for c in set(map(lambda chr_arm: chr_arm[0], pert_gene_chr_arm.values())):
         sorted_genes_on_chr[c] = list(avar[avar.chromosome == c].sort_values("start").index)
         start_block_of_chr[c] = anndat.uns["cnv"]["chr_pos"][c]
-        block_size_of_chr = len(sorted_genes_on_chr[c]) // blocksize
+        block_size_of_chr[c] = len(sorted_genes_on_chr[c]) // blocksize
 
     ko_gene_ixs_dict = {ko_gene: anndat.obs.gene == ko_gene for ko_gene in pert_genes}
     ko_gene_sum_dict = {ko_gene: sum(ixs) for ko_gene, ixs in ko_gene_ixs_dict.items()}
